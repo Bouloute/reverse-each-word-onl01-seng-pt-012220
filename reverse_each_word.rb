bc911ec4d74361d988adb29d87df9177c7f9bbf6
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 20:45:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   reversed_sentence = []
   letters = sentence.split("")
   letters.collect{ |letter|
-    reversed_sentence.
+    reversed_sentence.unshift(letter)
   }
   reversed_sentence.join()
 end
